2f8593e1264d1d6d2fa23ffac375218ebc5b8649
Michael Alt <mail@lean-stack.de>
2021-01-13 22:04:55 +0100

    Implement todos input.

--- src/components/TodosInput.vue
+++ src/components/TodosInput.vue
@@ -1,9 +1,23 @@
 <template>
-  <input class="new-todo" placeholder="What needs to be done?" autofocus />
+  <input v-model="title" @keyup.enter="createTodo"
+    class="new-todo" placeholder="What needs to be done?" autofocus />
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
-export default Vue.extend({});
+export default Vue.extend({
+  data() {
+    return { title: '' };
+  },
+  methods: {
+    createTodo() {
+      this.title = this.title.trim();
+      if (this.title) {
+        this.$emit('create', this.title);
+        this.title = '';
+      }
+    },
+  },
+});
 </script>
