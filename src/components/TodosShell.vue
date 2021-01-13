389cbb2863185ddc87f88a47b7e89298b7157d9e
Michael Alt <mail@lean-stack.de>
2021-01-13 22:59:40 +0100

    Handle initial loading and adding of new todos.

--- src/components/TodosShell.vue
+++ src/components/TodosShell.vue
@@ -2,7 +2,7 @@
   <section class="todoapp">
     <header class="header">
       <h1>todos</h1>
-      <todos-input />
+      <todos-input @create="createTodo" />
     </header>
     <todos-main />
     <todos-actionbar />
@@ -11,11 +11,18 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import { mapActions } from 'vuex';
 import TodosActionbar from './TodosActionbar.vue';
 import TodosInput from './TodosInput.vue';
 import TodosMain from './TodosMain.vue';
 
 export default Vue.extend({
   components: { TodosMain, TodosInput, TodosActionbar },
+  methods: {
+    ...mapActions(['createTodo']),
+  },
+  mounted() {
+    this.$store.dispatch('initialize');
+  },
 });
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,7 @@
+import { Todo } from '@/models/todo';
 import Vue from 'vue';
 import Vuex from 'vuex';
+import backend from '@/api/local-backend';
 import { State } from './state';
 
 Vue.use(Vuex);
@@ -13,8 +15,22 @@ export default new Vuex.Store<State>({
     hasTodos: (state) => state.todos.length > 0,
   },
   mutations: {
+    initialize: (state, todos: Todo[]) => {
+      state.todos = todos;
+    },
+    createTodo: (state, todo: Todo) => {
+      state.todos.push(todo);
+    },
   },
   actions: {
+    async initialize({ commit }) {
+      const todos = await backend.getAll();
+      commit('initialize', todos);
+    },
+    async createTodo({ commit }, title: string) {
+      const todo = await backend.createTodo(title);
+      commit('createTodo', todo);
+    },
   },
   modules: {
   },
