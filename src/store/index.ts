c3cbf6a2e24947c9c4120ceee4e359be70cb7fc3
Michael Alt <mail@lean-stack.de>
2021-01-13 21:38:03 +0100

    Add initial state to store.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { State } from './state';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+export default new Vuex.Store<State>({
   state: {
+    todos: [],
   },
   mutations: {
   },
