9d5d5180154527e8e138b2a32dcbee208665c2cd
Michael Alt <mail@lean-stack.de>
2021-01-15 21:35:28 +0100

    Update author info.

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,7 @@
     <router-view />
     <footer class="info">
       <p>Double-click to edit a todo</p>
-      <!-- Remove the below line ↓ -->
-      <p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
-      <!-- Change this out with your name and url ↓ -->
-      <p>Created by <a href="http://todomvc.com">you</a></p>
+      <p>Created by <a href="https://lean-stack.de">Micha Alt</a></p>
       <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
     </footer>
   </div>
