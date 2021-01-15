0043da8a456629dc74c4f07764cdbb2da3889775
Michael Alt <mail@lean-stack.de>
2021-01-15 21:59:49 +0100

    Update actionbar. Remove deprecated filter.

--- src/components/TodosActionbar.vue
+++ src/components/TodosActionbar.vue
@@ -2,7 +2,7 @@
   <footer v-show="hasTodos" class="footer">
     <!-- This should be `0 items left` by default -->
     <span class="todo-count"><strong>{{ activeCount }}</strong>
-      {{ 'item' | pluralize(activeCount) }} left
+      {{ activeCount === 1 ? 'item' : 'items' }} left
     </span>
     <!-- Remove this if you don't implement routing -->
     <ul class="filters">
@@ -29,20 +29,14 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
 import { mapActions, mapGetters } from 'vuex';
 
-export default Vue.extend({
+export default {
   computed: {
     ...mapGetters(['activeCount', 'hasTodos', 'hasCompletedTodos', 'isVisibility']),
   },
   methods: {
     ...mapActions(['deleteCompletedTodos']),
   },
-  filters: {
-    pluralize(word: string, count: number): string {
-      return count === 1 ? word : `${word}s`;
-    },
-  },
-});
+};
 </script>
